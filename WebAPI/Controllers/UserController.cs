f0007eaa21540d4e1c711e122f3a4e9f2d6dbcf6
Shev <125613450+FrancescoTalento@users.noreply.github.com>
2025-09-21 20:41:51 -0300

    Basic Startup Files

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data.DTO;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(CreateUserDto createUser)
+        {
+            throw new NotImplementedException();    
+        }
+    }
+
+
+}
---
+++ WebAPI/Data/DTO/CreateUserDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Data.DTO
+{
+    public record CreateUserDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+
+    }
+}
